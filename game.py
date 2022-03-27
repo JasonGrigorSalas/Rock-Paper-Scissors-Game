d5549b942c28b1551f5c7fb5d4cc992c996390a3
JasonGrigorSalas <jasoncoco1@gmail.com>
2022-03-26 22:44:30 -0400

    Added new game file.

---
+++ game.py
@@ -0,0 +1,5 @@
+choices = ["rock", "paper", "scissors"]
+# player_choice = choices[0]
+# print("Index 0 in the choice array is " + player_choice + ", which is Rock.")
+player_choice = input("Choose rock, paper, or scissors: ")
+print("User chose " + player_choice)
