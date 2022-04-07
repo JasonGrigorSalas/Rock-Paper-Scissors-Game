7465ecc2b13ee09ed94290e85620cf22bc042aba
JasonGrigorSalas <jasoncoco1@gmail.com>
2022-04-07 13:30:48 -0400

    Edited the game file.

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ def win_or_lose(status):
     print("You ", status, " Would you like to play again?")
     choice = input("Y/N ")
     if choice == "N" or choice == "n":
-        print("Hope you play again soon! Farewell! :D")
+        print("Hope you play again soon! Farewell! :D \n")
         exit()
     elif choice == "Y" or choice == "y":
         global player_lives
@@ -17,56 +17,56 @@ def win_or_lose(status):
         player_lives = total_lives
         computer_lives = total_lives
     else:
-        print(" ._. ")
+        print(" ._. \n")
         choice = input("Y/N ")
 
 while player_choice is False:
     print("******** Sword Shield Fist ********")
     print("Computer lives: ", computer_lives, "/", total_lives)
     print("Player lives: ", player_lives, "/", total_lives)
-    print("***********************************")
-    print("Choose your weapon! Or, if ye be a coward, type quit to exit.")
+    print("***********************************\n")
+    print("Choose your weapon! Or, if ye be a coward, type quit to exit. \n")
     player_choice = input("Choose sword, shield, or fist: ")
     if player_choice == "quit":
-        print("Hope you play again soon! Farewell! :D")
+        print("Hope you play again soon! Farewell! :D \n")
         exit()
 
     print("User chose: " + player_choice)
     computer_choice = choices[randint(0, 2)]
     print("Computer chose: " + computer_choice)
     if computer_choice == player_choice:
-        print("Tie. :/")
+        print("Tie. :/ \n")
 
     elif computer_choice == "sword":
         if player_choice == "fist":
-            print("You lose! :(")
+            print("You lose! :( \n")
             # player_lives = player_lives - 1
             player_lives -= 1
         else:
-            print("You win! :)")
+            print("You win! :) \n")
             computer_lives -= 1
 
     elif computer_choice == "shield":
         if player_choice == "sword":
-            print("You lose! :(")
+            print("You lose! :( \n")
             player_lives -= 1
         else:
-            print("You win! :)")
+            print("You win! :) \n")
             computer_lives -= 1
 
     elif computer_choice == "fist":
         if player_choice == "shield":
-            print("You lose! :(")
+            print("You lose! :( \n")
             player_lives -= 1
         else:
-            print("You win! :)")
+            print("You win! :) \n")
             computer_lives -= 1
 
     if player_lives == 0:
-        win_or_lose("lose! :(")
+        win_or_lose("lose! :( \n")
 
     if computer_lives == 0:
-        win_or_lose("win! :)")
+        win_or_lose("win! :) \n")
 
     print("Player lives: ", player_lives)
     print("Computer lives: ", computer_lives)
